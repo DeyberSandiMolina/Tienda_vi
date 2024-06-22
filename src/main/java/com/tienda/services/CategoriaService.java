c847cfea16f5eac573515ea0656925337c0f300b
fabia <fabia@Deyber_desktop>
2024-06-22 12:21:04 -0600

    Tarea 5: SQL

---
+++ src/main/java/com/tienda/services/CategoriaService.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.tienda.services;
+
+import com.tienda.domain.Categoria;
+import java.util.List;
+
+/**
+ *
+ * @author fabia
+ */
+public interface CategoriaService {
+    /*Se recusperan los registro de la table categoria en un 
+    arrayList de objetos categoria. 
+    Solo los activos*/
+    public List<Categoria> getCategorias(boolean activos);
+}
