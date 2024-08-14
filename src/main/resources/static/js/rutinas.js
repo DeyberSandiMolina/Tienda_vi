1deebe948ce5714ccd89231567bcbc4b081df462
fabia <fabia@Deyber_desktop>
2024-08-14 14:44:39 -0600

    Semana 12: correos

--- src/main/resources/static/js/rutinas.js
+++ src/main/resources/static/js/rutinas.js
@@ -14,3 +14,14 @@ function readURL(input){
     }
     
 }
+
+function addCart(formulario) {
+    
+    var idProducto = formulario.elements[0].value;
+    var existencias = formulario.elements[1].value;
+    window.alert("los parametros son "+idProducto+ " y "+ existencias);
+    if (existencias > 0) {
+        var url = "/carrito/agregar/"+idProducto;
+        $("#resultsBlock").load(url);
+    }
+}
